8aabb86de30eefe9d7359fbbd2dfb2b5638d3333
bveinz <velosobruno61@gmail.com>
2024-08-13 16:38:23 -0400

    FINISH SIMPLE AUTHENTICATION APP

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
@@ -8,6 +8,8 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
 import IconButton from './components/ui/IconButton'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading';
 
 
 const Stack = createNativeStackNavigator();
@@ -62,12 +64,41 @@ function Navigation() {
   );
 }
 
+
+function Root(){
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+  const authCtx = useContext(AuthContext)
+
+  useEffect( () => {
+    async function fetchToken(){
+        const storedToken = await AsyncStorage.getItem('token');
+
+        if (storedToken){
+            authCtx.authenticate(storedToken)
+        }
+
+        setIsTryingLogin(false);
+    }
+    fetchToken();
+  },[])
+
+
+  if (isTryingLogin){
+    return <AppLoading/>
+  }
+
+  return <Navigation/>
+
+} 
+
+
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );

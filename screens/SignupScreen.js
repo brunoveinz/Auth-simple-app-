6228d8d09fa41e2986cbbc20b67ed94b528354d8
bveinz <velosobruno61@gmail.com>
2024-08-13 10:49:01 -0400

    adding sucessfully login

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay'
 import { createUser } from '../util/auth';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context'
 
 function SignupScreen() {
-
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
 
   async function signupHandler({email, password}){
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      const token = await createUser(email, password);    
+      authCtx.authenticate(token)  
+    } catch (error) {
+      Alert.alert('Authentication Failed','Could not create user, please check your input and try again later')
+    }
     setIsAuthenticating(false)
   }
 
   if (isAuthenticating) {
-    return <LoadingOverlay message="Creating user..."/>
+    return <LoadingOverlay message="Creating user..." />
   }
 
 
--- util/auth.jsx
+++ util/auth.jsx
@@ -2,18 +2,31 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyACM0mVPbrvNt8H5Xo8LrXNXEgd6xq1fps';
 
-export async function createUser(email, password) {
-    try {
-        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        );
-        return response.data; // Devolver la respuesta para poder usarla en otra parte del código
-    } catch (error) {
-        console.error('Error creating user:', error);
-        throw error; // Lanzar el error para que se pueda manejar en otra parte del código si es necesario
-    }
+
+async function authenticate(mode, email, password){
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
+
+    const response = await axios.post(url, {
+       email : email,
+       password : password,
+       returnSecureToken : true 
+    })
+
+    const token = response.data.idToken;
+    return token;
 }
+
+
+export function login(email, password){
+    return authenticate('signInWithPassword', email, password) 
+}
+
+export function createUser(email, password){
+    return authenticate('signUp', email, password)
+}
+
+
+
+
+
+
